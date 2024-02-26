76a258a485971312b7cd0fc266375fca81ede842
KingDavid <ezehdavidifeanyi1234@gmail.com>
2024-02-26 20:25:34 +0100

    update user routes

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,1 +1,11 @@
 const express = require('express')
+
+const router = express.Router()
+
+const {signin, signup } = require('../controllers/user.controller')
+
+router.route('/signup').post(signup)
+router.route('/signin').post(signin)
+
+
+module.exports = router

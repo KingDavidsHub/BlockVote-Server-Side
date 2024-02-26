9a1506b4b00c20597b464df3fa15d4c5e4cb3250
KingDavid <ezehdavidifeanyi1234@gmail.com>
2024-02-26 20:43:25 +0100

    update file changes

--- routes/organization.router.js
+++ routes/organization.router.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-const { signup, signin, deleteAllOrganizations} = require('../controllers/organization.controller')
+const { signup, signin, deleteAllOrganizations} = require('../controllers/organization.auth')
 
 
 router.route('/signup').post(signup)
--- routes/user.router.js
+++ routes/user.router.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-const {signin, signup } = require('../controllers/user.controller')
+const {signin, signup } = require('../controllers/user.auth')
 
 router.route('/signup').post(signup)
 router.route('/signin').post(signin)

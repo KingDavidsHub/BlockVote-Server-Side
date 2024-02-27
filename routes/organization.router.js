e32b454d5ab565e1778f8e09d291d988e22039f2
KingDavid <ezehdavidifeanyi1234@gmail.com>
2024-02-27 18:48:15 +0100

    update

--- routes/organization.router.js
+++ routes/organization.router.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-const { signup, signin, deleteAllOrganizations} = require('../controllers/organization.auth')
+const { signup, signin, deleteAllOrganizations} = require('../controllers/organization.auth.controller')
 
 
 router.route('/signup').post(signup)
--- routes/user.router.js
+++ routes/user.router.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-const {signin, signup } = require('../controllers/user.auth')
+const {signin, signup } = require('../controllers/user.auth.controller')
 
 router.route('/signup').post(signup)
 router.route('/signin').post(signin)

9abd32945b72cc224e0a4cd2721bbdf48c92113b
KingDavid <ezehdavidifeanyi1234@gmail.com>
2024-03-13 08:02:32 +0100

    update

--- routes/election.router.js
+++ routes/election.router.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 const { createElection, populateCandidates, registerForElection, getElection} = require('../controllers/election.controller')
 router.route("/createElection").post(createElection)
-router.route('/getElection').get(getElection)
+router.route('/getElection/:electionId').get(getElection)
 router.route('/populateCandidate').put(populateCandidates)
 router.route('/registerForElection').post(registerForElection)
 

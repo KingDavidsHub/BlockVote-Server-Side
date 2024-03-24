8bc6b8324e9b9192e9b023c249f60567e9d14292
Ibe Praise <praiseibec@gmail.com>
2024-03-24 04:45:07 +0100

    update elections

--- controllers/election.controller.js
+++ controllers/election.controller.js
@@ -1,83 +1,110 @@
-const Election = require('../models/election.model')
+const Election = require("../models/election.model");
 
+exports.createElection = async (req, res) => {
+  try {
+    const election = await new Election({
+      ...req.body,
+    }).save();
 
-exports.createElection = async (req,res) =>{
-    try {
-        const election = await new Election({
-           ...req.body 
-        }).save()
+    res.status(200).json({
+      success: true,
+      data: election,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
-        res.status(200).json({
-            success: true,
-            data: election
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message:error.message
-        })
+exports.populateCandidates = async (req, res) => {
+  try {
+    const election = await Election.findById(req.params.electionId).populate(
+      "candidates"
+    );
+
+    re.status(200).json({
+      success: true,
+      data: election,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.registerForElection = async (req, res) => {
+  try {
+    const { email, firstname, lastname } = req.body;
+
+    const election = await Election.findByIdAndUpdate(
+      req.params.electionId,
+      {
+        $push: {
+          registeredVoters: { email, firstname, lastname, isVerified: false },
+        },
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      data: election,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.getElection = async (req, res) => {
+  try {
+    const election = await Election.findById(req.params.electionId);
+
+    res.status(200).json({
+      success: true,
+      data: election,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.getAllElections = async (req, res) => {
+  try {
+    const elections = await Election.find({});
+
+    if (elections.length !== 0) {
+      res.status(200).json({
+        success: true,
+        data: elections,
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "elections not found",
+      });
     }
-}
-
-exports.populateCandidates = async(req,res) =>{
-    try {
-         const election =await Election.findById(req.params.electionId).populate("candidates");
-
-         re.status(200).json({
-            success: true,
-            data: election
-         })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: error.message
-        })
-    }
-}
-
-
-exports.registerForElection = async(req,res) =>{
-    try {
-        const { email, firstname, lastname} = req.body
-
-       const election = await Election.findByIdAndUpdate(req.params.electionId, {
-            $push:{registeredVoters: {email,firstname,lastname, isVerified: false}}
-        }, {
-            new: true,
-            runValidators: true
-        })
-
-        res.status(200).json({
-            success: true,
-            data: election
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: error.message
-        })
-    }
-}
-
-
-exports.getElection = async(req,res) =>{
-    try {
-        const election =  await Election.findById(req.params.electionId)
-
-
-        res.status(200).json({
-            success: true,
-            data: election
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: error.message
-        })
-    }
-}
-
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- routes/election.router.js
+++ routes/election.router.js
@@ -1,13 +1,18 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
+const {
+  createElection,
+  populateCandidates,
+  registerForElection,
+  getElection,
+  getAllElections,
+} = require("../controllers/election.controller");
+router.route("/createElection").post(createElection);
+router.route("/getElection/:electionId").get(getElection);
+router.route("/getAllElections").get(getAllElections);
+router.route("/populateCandidate").put(populateCandidates);
+router.route("/registerForElection/:electionId").post(registerForElection);
 
-const { createElection, populateCandidates, registerForElection, getElection} = require('../controllers/election.controller')
-router.route("/createElection").post(createElection)
-router.route('/getElection/:electionId').get(getElection)
-router.route('/populateCandidate').put(populateCandidates)
-router.route('/registerForElection/:electionId').post(registerForElection)
-
-
-module.exports = router
+module.exports = router;
